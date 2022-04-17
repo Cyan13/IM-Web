52faf5ce1540c16539473ff25e4f9ec786bddfac
Cyan13 <shenqi_endless@sina.cn>
2022-04-17 13:55:20 +0800

    logoutfix

--- src/views/helper/index.vue
+++ src/views/helper/index.vue
@@ -99,6 +99,7 @@ export default {
       this.$store1.state.username=''
       this.$store1.state.userid=''
       this.$store1.state.schedule=''
+      this.$store1.state.role=''
       this.$router.push({
         path: '/index'
       })
